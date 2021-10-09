d1e3e78a16d8a5898a2005345cda5393107e0fb1
Ole Frandsen <froksen@gmail.com>
2021-10-09 21:25:03 +0200

    Implementeret tidszone del i eventmanager

--- src/eventmanager.py
+++ src/eventmanager.py
@@ -36,35 +36,6 @@ class EventManager:
             sys.exit()
             return
 
-    def is_in_daylight(self, date_to_check):
-        #TODO: Find en smartere måde, at lave dette tjek på!
-        daylight_periods = [
-                {
-                    "start": dt.datetime(2022,3,27), #Den første dag i sommertid
-                    "end" : dt.datetime(2022,10,29) #Den sidste dag i sommertid
-                },
-                {
-                    "start": dt.datetime(2023,3,26), #Den første dag i sommertid
-                    "end" : dt.datetime(2023,10,28) #Den sidste dag i sommertid
-                },
-                {
-                    "start": dt.datetime(2024,3,31), #Den første dag i sommertid
-                    "end" : dt.datetime(2024,10,27) #Den sidste dag i sommertid
-                },
-                {
-                    "start": dt.datetime(2025,3,30), #Den første dag i sommertid
-                    "end" : dt.datetime(2025,10,26) #Den sidste dag i sommertid
-                },
-            ]
-
-        is_daylight = False
-        for daylight_period in daylight_periods:
-            if date_to_check <= daylight_period["end"] and date_to_check >= daylight_period["start"]:
-                is_daylight = True
-                break
-
-        return is_daylight
-
     def aula_event_update(self,obj):
         event_title = obj["appointmentitem"].subject
         start_date = obj["aula_startdate"]
@@ -175,7 +146,9 @@ class EventManager:
         for event_to_create in changes['events_to_create']:
             event_title = event_to_create["appointmentitem"].subject
             start_date = event_to_create["aula_startdate"]
+            start_date_timezone = event_to_create["aula_startdate_timezone"]
             end_date = event_to_create["aula_enddate"]
+            end_date_timezone = event_to_create["aula_enddate_timezone"]
             start_time = event_to_create["aula_starttime"]
             end_time = event_to_create["aula_endtime"]
             allDay = event_to_create["appointmentitem"].AllDayEvent
@@ -184,8 +157,8 @@ class EventManager:
                 start_dateTime = str(start_date).replace("/","-")  # FORMAT: 2021-05-18
                 end_dateTime = str(end_date).replace("/","-")  # FORMAT: 2021-05-18T15:00:00+02:00 2021-05-20
             else:
-                start_dateTime = str(start_date).replace("/","-") + "T" + start_time + "+02:00"  # FORMAT: 2021-05-18T15:00:00+02:00
-                end_dateTime = str(end_date).replace("/","-") + "T" + end_time + "+02:00" # FORMAT: 2021-05-18T15:00:00+02:00 2021-05-20T19:45:01T+02:00
+                start_dateTime = str(start_date).replace("/","-") + "T" + start_time + start_date_timezone  # FORMAT: 2021-05-18T15:00:00+02:00
+                end_dateTime = str(end_date).replace("/","-") + "T" + end_time + end_date_timezone # FORMAT: 2021-05-18T15:00:00+02:00 2021-05-20T19:45:01T+02:00
 
             location = event_to_create["appointmentitem"].location 
             sensitivity = event_to_create["appointmentitem"].Sensitivity 
