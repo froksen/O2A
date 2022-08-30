be64494b4a2dd28ac7882d984addfd8fbc302a22
Ole Frandsen <froksen@gmail.com>
2022-08-30 16:22:18 +0200

    Opdateret API til 14

--- src/aulamanager.py
+++ src/aulamanager.py
@@ -50,7 +50,7 @@ class AulaManager:
         return self.session
 
     def getAulaApiUrl(self):
-        return 'https://www.aula.dk/api/v13/'
+        return 'https://www.aula.dk/api/v14/'
 
     def getEventsForInstitutions(self,profileId,instCodes, startDateTime, endDateTime):
         session = self.getSession()
