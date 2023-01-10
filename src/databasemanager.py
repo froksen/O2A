36aa73150ed7f883a17d21f5b0e821d2cee1e174
Ole Frandsen <froksen@gmail.com>
2023-01-10 09:38:22 +0100

    Første udgave af database

--- src/databasemanager.py
+++ src/databasemanager.py
@@ -19,15 +19,27 @@ class DatabaseManager:
 
     def create_tables(self):
         # Create operation
-        create_query = '''CREATE TABLE "tblEvents" (
-                "id"	INTEGER,
-                "outlook_id"	TEXT,
-                "aula_id"	TEXT,
-                "created"	TEXT,
-                "updated"	TEXT,
-                PRIMARY KEY("id" AUTOINCREMENT)
-            );
-        '''
-        self.cursor.execute(create_query)
-        print("Table created!")
+        try:
+            create_query = '''CREATE TABLE "tblEvents" (
+                    "id"	INTEGER,
+                    "outlook_id"	TEXT,
+                    "aula_id"	TEXT,
+                    "created"	TEXT,
+                    "updated"	TEXT,
+                    PRIMARY KEY("id" AUTOINCREMENT)
+                );
+            '''
+            self.cursor.execute(create_query)
+            print("Table created!")
+        except sqlite3.OperationalError as e:
+            print(e)
 
+    def get_record(self, outlook_id):
+        command = 'SELECT * FROM tblEvents WHERE outlook_id="?"'
+        cursor = self.conn.cursor()
+        records = cursor.execute(command, (outlook_id))
+
+        return records
+
+    def update_record(self, outlook_id, aula_id):
+        pass
