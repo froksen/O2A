4469a6ab273d3b65bbd77bab4220d91e19235917
Ole Holm Frandsen <froksen@gmail.com>
2023-11-04 21:27:41 +0100

    Ny udgivelse med ny versionsnr.

--- outlook_addin/src/Ribbon1.cs
+++ outlook_addin/src/Ribbon1.cs
@@ -18,7 +18,7 @@ namespace Outlook2Aula
 
             Globals.Ribbons.Ribbon1.lblO2APath.Label = getO2AFolderPath();
 
-            Globals.Ribbons.Ribbon1.lblBuildVersion.Label = "2023-11-02"; //Environment.GetEnvironmentVariable("ClickOnce_CurrentVersion");
+            Globals.Ribbons.Ribbon1.lblBuildVersion.Label = "2023-11-04"; //Environment.GetEnvironmentVariable("ClickOnce_CurrentVersion");
 
             //Oprindeligt fra: https://robindotnet.wordpress.com/2010/07/11/how-do-i-programmatically-find-the-deployed-files-for-a-vsto-add-in/
             //Get the assembly information
